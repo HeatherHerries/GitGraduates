1e98d982102d080ad90c8e2af60525a455eed0c2
Tparker85 <pawka8855@gmail.com>
2022-04-03 19:41:25 -0600

    Take 2

---
+++ JS/gitGrad.js
@@ -0,0 +1,28 @@
+let gitGrads = [
+{id: 1,
+       Name:`TerranceP`,
+       Style:`Javascript`,
+       Age:`36`},
+       
+{id:2, 
+      Name:`HeatherH`,
+ Style: `Html and CSS`,
+ Age:`43`,
+}
+]
+
+
+gitGrads.forEach((gitGrads) => {
+      return console.log(gitGrads
+
+      );
+});
+
+
+
+for (let i = 0; i <=gitGrads.length; i++) {
+      const gitGrads =[i];
+      
+}
+
+console.log(gitGrads);
